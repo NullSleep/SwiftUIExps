52f27a6a93881eba51fa43b1dd987a7b0899a97d
Carlos Arenas <carlos.arenas@virtual-solution.com>
2022-12-27 22:59:35 +0100

    Some customization

--- Apple Developer SwiftUI/SwiftUI Essentials/CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
+++ Apple Developer SwiftUI/SwiftUI Essentials/CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 
+// BOKU: By default, SwiftUI view files declare two structures. The first structure conforms to the View protocol and describes the view’s content and layout. The second structure declares a preview for that view.
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hallo, welt!")
+            Text("Geil dinge").foregroundColor(.green)
         }
         .padding()
     }
