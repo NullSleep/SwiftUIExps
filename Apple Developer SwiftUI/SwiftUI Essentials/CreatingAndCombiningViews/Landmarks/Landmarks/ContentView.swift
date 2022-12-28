96d2882f8ea6b4fb439087f71f35303614a24b22
Carlos Arenas <carlos.arenas@virtual-solution.com>
2022-12-28 13:35:55 +0100

    Update to the UI and documentation

--- Apple Developer SwiftUI/SwiftUI Essentials/CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
+++ Apple Developer SwiftUI/SwiftUI Essentials/CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -7,7 +7,11 @@
 
 import SwiftUI
 
-// BOKU: By default, SwiftUI view files declare two structures. The first structure conforms to the View protocol and describes the view’s content and layout. The second structure declares a preview for that view.
+// GUIDE: By default, SwiftUI view files declare two structures. The first structure conforms to the View protocol and describes the view’s content and layout. The second structure declares a preview for that view.
+
+// GUIDE: To customize a SwiftUI view, you call methods called modifiers. Modifiers wrap a view to change its display or other properties. Each modifier returns a new view, so it’s common to chain multiple modifiers, stacked vertically.
+
+// GUIDE: Your code is always the source of truth for the view. When you use the inspector to change or remove a modifier, Xcode updates your code immediately to match.
 
 struct ContentView: View {
     var body: some View {
@@ -15,7 +19,10 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hallo, welt!")
+            Text("Munich, Bavaria")
+                .font(.title)
+                .padding()
+                .foregroundColor(.black)
             Text("Geil dinge").foregroundColor(.green)
         }
         .padding()
