a5744e0e288abd5a7a629a999673945bda55b4a2
NullSleep <carenasdev@gmail.com>
2023-01-18 15:15:39 +0100

    Adding koordinaten structures and data

--- Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Wahrzeichen.swift
+++ Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Wahrzeichen.swift
@@ -8,9 +8,10 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
 
-// GUIDE: Define a Landmark (Wahrzeichen) structure with a few properties matching names of some of the keys in the landmarkData.json data file.
-// Adding Codable conformance makes it easier to move data between the structure and a data file. You’ll rely on the Decodable component of the Codable protocol later in this section to read data from file.
+// LEITFADEN: Define a Landmark (Wahrzeichen) structure with a few properties matching names of some of the keys in the landmarkData.json data file.
+// LEITFADEN: Adding Codable conformance makes it easier to move data between the structure and a data file. You’ll rely on the Decodable component of the Codable protocol later in this section to read data from file.
 
 // Wahrzeichen = Punto destacado = Landmark 
 struct Wahrzeichen: Hashable, Codable {
@@ -21,7 +22,22 @@ struct Wahrzeichen: Hashable, Codable {
   var description: String
   
   private var bildName: String
+  
   var bild: Image {
     Image(bildName)
   }
+  
+  private var koordinaten: Koordinaten
+  
+  // LEITFADEN: Compute a locationCoordinate property that’s useful for interacting with the MapKit framework.
+  var ortsKoordinate: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(latitude: koordinaten.breitengrad, longitude: koordinaten.länge)
+  }
+  
+  
+  struct Koordinaten: Hashable, Codable {
+    var breitengrad: Double
+    var länge: Double
+  }
+  
 }
