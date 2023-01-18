bee00a4d17af43bc895a202cbdcc9da0e69ec512
NullSleep <carenasdev@gmail.com>
2023-01-18 14:14:18 +0100

    Adding images to the wahrzeichen structure and images to the project assets

--- Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Wahrzeichen.swift
+++ Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Wahrzeichen.swift
@@ -7,14 +7,21 @@
 //
 
 import Foundation
+import SwiftUI
 
 // GUIDE: Define a Landmark (Wahrzeichen) structure with a few properties matching names of some of the keys in the landmarkData.json data file.
 // Adding Codable conformance makes it easier to move data between the structure and a data file. You’ll rely on the Decodable component of the Codable protocol later in this section to read data from file.
 
+// Wahrzeichen = Punto destacado = Landmark 
 struct Wahrzeichen: Hashable, Codable {
   var id: Int
   var name: String
   var park: String
   var state: String
   var description: String
+  
+  private var bildName: String
+  var bild: Image {
+    Image(bildName)
+  }
 }
