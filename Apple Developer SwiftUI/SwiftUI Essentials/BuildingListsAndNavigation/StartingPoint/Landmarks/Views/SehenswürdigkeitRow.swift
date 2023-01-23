0a446775f17a6a30b2e5f5a55c942034cf51026d
NullSleep <carenasdev@gmail.com>
2023-01-23 01:31:36 +0100

    Customizing the SehenswürdigkeitRow

--- Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Views/SehenswürdigkeitRow.swift
+++ Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Views/SehenswürdigkeitRow.swift
@@ -12,10 +12,18 @@ struct SehenswürdigkeitRow: View {
   var sehenswürdigkeit: Sehenswürdigkeit
   
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    HStack {
+      sehenswürdigkeit.bild
+        .resizable()
+        .frame(width: 50,  height: 50)
+      Text(sehenswürdigkeit.name)
+      Spacer()
+    }
   }
 }
 
+// LEITFADEN: Xcode’s canvas automatically recognizes and displays any type in the current editor that conforms to the PreviewProvider protocol. A preview provider returns one or more views, with options to configure the size and device.
+// LEITFADEN: You can customize the returned content from a preview provider to render exactly the previews that are most helpful to you.
 struct WahrzeichenRow_Previews: PreviewProvider {
   static var previews: some View {
     SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[0])
