bf21ae3e46022cabe3f541beb45778707338c891
NullSleep <carenasdev@gmail.com>
2023-01-24 02:53:16 +0100

    Customizing and grouping the row preview

--- Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Views/SehenswürdigkeitRow.swift
+++ Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Views/SehenswürdigkeitRow.swift
@@ -26,6 +26,20 @@ struct SehenswürdigkeitRow: View {
 // LEITFADEN: You can customize the returned content from a preview provider to render exactly the previews that are most helpful to you.
 struct WahrzeichenRow_Previews: PreviewProvider {
   static var previews: some View {
-    SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[0])
+    
+    // LEITFADEN: You can use a Group to return multiple previews from a preview provider. Group is a container for grouping view content. Xcode renders the group’s child views as separate previews in the canvas.
+//    Group {
+//      SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[3])
+//        .previewLayout(.fixed(width: 300, height: 70))
+//      SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[1])
+//        .previewLayout(.fixed(width: 300, height: 70))
+//    }
+    // Simplified version
+    Group {
+      SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[3])
+      SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[1])
+    }
+    // A view’s children inherit the view’s contextual settings, such as preview configurations.
+    .previewLayout(.fixed(width: 300, height: 70))
   }
 }
