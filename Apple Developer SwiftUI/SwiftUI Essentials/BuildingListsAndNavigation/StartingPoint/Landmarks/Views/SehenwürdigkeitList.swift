35f99ab5f32f421c4ae1eda3ae43687515f8ccc7
NullSleep <carenasdev@gmail.com>
2023-01-27 13:07:51 +0100

    Adding a dynamically-generated list of landmarks

--- Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Views/SehenwürdigkeitList.swift
+++ Apple Developer SwiftUI/SwiftUI Essentials/BuildingListsAndNavigation/StartingPoint/Landmarks/Views/SehenwürdigkeitList.swift
@@ -12,9 +12,15 @@ import SwiftUI
 
 struct SehenswürdigkeitList: View {
     var body: some View {
-      List {
-        SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[4])
-        SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[2])
+//      List {
+//        SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[4])
+//        SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeiten[2])
+//      }
+        
+      // LEITFADEN: One can create a list that displays the elements of a collection by passing your collection of data and a closure that provides a view for each element in the collection. The list transforms each element in the collection into a child view by using the supplied closure.
+      // LEITFADEN: Pass the model data’s landmarks array to the List initializer. Lists work with identifiable data. You can make your data identifiable in one of two ways: by passing along with your data a key path to a property that uniquely identifies each element, or by making your data type conform to the Identifiable protocol.
+      List(sehenswürdigkeiten, id: \.id) { sehenswürdigkeit in
+        SehenswürdigkeitRow(sehenswürdigkeit: sehenswürdigkeit)
       }
     }
 }
