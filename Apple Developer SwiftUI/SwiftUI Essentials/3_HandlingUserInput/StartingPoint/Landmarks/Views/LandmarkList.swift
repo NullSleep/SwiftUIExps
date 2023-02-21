0a2e00c6b761ddf5fa6df03b07513e138ca75ad2
NullSleep <carenasdev@gmail.com>
2023-02-21 01:01:15 +0100

    Filtering the list view

--- Apple Developer SwiftUI/SwiftUI Essentials/3_HandlingUserInput/StartingPoint/Landmarks/Views/LandmarkList.swift
+++ Apple Developer SwiftUI/SwiftUI Essentials/3_HandlingUserInput/StartingPoint/Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,19 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
+    // LEITFADEN: State is a value, or a set of values, that can change over time, and that affects a view’s behavior, content, or layout. You use a property with the @State attribute to add state to a view.
+    // LEITFADEN: Changing this value will trigger the preview to change the landmarks it will show
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
@@ -24,10 +34,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+//        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+//            LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
+        LandmarkList()
     }
 }
