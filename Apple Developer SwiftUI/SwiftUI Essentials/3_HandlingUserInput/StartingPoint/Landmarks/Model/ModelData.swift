aece5d0ed23fba33dc5ae5b72349a66b2fde60e7
NullSleep <carenasdev@gmail.com>
2023-02-26 02:31:56 +0100

    Use an Observable Object for Storage

--- Apple Developer SwiftUI/SwiftUI Essentials/3_HandlingUserInput/StartingPoint/Landmarks/Model/ModelData.swift
+++ Apple Developer SwiftUI/SwiftUI Essentials/3_HandlingUserInput/StartingPoint/Landmarks/Model/ModelData.swift
@@ -6,8 +6,14 @@ Storage for model data.
 */
 
 import Foundation
+import Combine
 
-var landmarks: [Landmark] = load("landmarkData.json")
+// LEITFADEN: An observable object is a custom object for your data that can be bound to a view from storage in SwiftUI’s environment. SwiftUI watches for any changes to observable objects that could affect a view, and displays the correct version of the view after a change.
+// LEITFADEN: SwiftUI subscribes to your observable object, and updates any views that need refreshing when the data changes.
+final class ModelData: ObservableObject {
+    // LEITFADEN: An observable object needs to publish any changes to its data, so that its subscribers can pick up the change.
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
